6db59ed8b0c8152f3e42bb87c88d0c988ae6f237
Brendon Pinniger <brendon.pinniger@zoom.us>
2025-06-24 09:48:21 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const crypto = require('crypto')
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.status(200)
+  res.send(`Zoom Webhook app successfully running.`)
+})
+
+app.listen(port, () => console.log(`Zoom Webhook sample listening on port ${port}!`))
